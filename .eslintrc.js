aafef337577db8a5cdc32ca482653681d8b93ff3
cms67 <a24985693@gmail.com>
2024-07-19 16:24:29 +0800

    add dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 'off',
+    'vuejs-accessibility/label-has-for': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vuejs-accessibility/form-control-has-label': 'off',
+    'func-names': 'off',
+    'vuejs-accessibility/click-events-have-key-events': 'off',
   },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,119 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginPage.vue'),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/Dashboard.vue'),
+    children: [
+      {
+        path: 'products',
+        name: 'products',
+        component: () => import('../views/Products.vue'),
+      },
+      {
+        path: 'orders',
+        name: 'orders',
+        component: () => import('../views/Orders.vue'),
+      },
+      {
+        path: 'coupon',
+        name: 'coupon',
+        component: () => import('../views/Coupon.vue'),
+      },
+      {
+        path: 'articles',
+        name: 'articles',
+        component: () => import('../views/Articles.vue'),
+      },
+    ],
   },
+  {
+    path: '/user',
+    name: 'user',
+    component: () => import('../views/Userboard.vue'),
+    children: [
+      {
+        path: 'index',
+        name: 'index',
+        component: () => import('../views/Index.vue'),
+      },
+      {
+        path: 'productsList',
+        name: 'productsList',
+        component: () => import('../views/UserProducts.vue'),
+      },
+      {
+        path: 'product/:productId',
+        name: 'product',
+        component: () => import('../views/UserProduct.vue'),
+      },
+      {
+        path: 'articlesList',
+        name: 'articlesList',
+        component: () => import('../views/UserArticles.vue'),
+      },
+      {
+        path: 'article/:id',
+        name: 'article',
+        component: () => import('../views/UserArticle.vue'),
+      },
+      {
+        path: 'favorite',
+        name: 'favorite',
+        component: () => import('../views/UserFavorite.vue'),
+      },
+      {
+        path: 'cart',
+        name: 'cart',
+        component: () => import('../views/UserCart.vue'),
+      },
+      {
+        path: 'information',
+        name: 'information',
+        component: () => import('../views/UserInformation.vue'),
+      },
+      {
+        path: 'checkout/:orderId',
+        name: 'checkout',
+        component: () => import('../views/UserCheckout.vue'),
+      },
+      {
+        path: 'inquire',
+        name: 'inquire',
+        component: () => import('../views/UserInquire.vue'),
+      },
+    ],
+  },
+  // {
+  //   path: '/:pathMatch(.*)*',
+  //   redirect: {
+  //     name: 'index',
+  //   },
+  // },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(process.env.VUE_APP_BASE_URL),
+  base: process.env.BASE_URL,
   routes,
+  linkActiveClass: 'active',
+  scrollBehavior(to) {
+    // console.log(to, from, savedPosition);
+    if (to.fullPath.match('user')) {
+      return {
+        top: 0,
+      };
+    }
+    return {};
+    // `to` 和 `from` 都是路由路径
+    // `savedPosition` 如果不存在可以为 null
+  },
 });
 
 export default router;
